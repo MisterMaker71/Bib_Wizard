bc8dd3ac95e1cefd656e047ede306a7ce15f4bf1
MisterMaker71 <Mister.Maker_71@gmx.de>
2024-06-19 10:58:26 +0200

    made changes to the 3D world. pause menu

---
+++ Assets/Scripts/Interactor.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactor : MonoBehaviour
+{
+    public UnityEvent Interact;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,163 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+//[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    public float MovementSpeed = 5;
+    public float JumpPower = 2.5f;
+    public static CharacterController controller;
+    public LayerMask GroundLayer;
+    //public Rigidbody rb;
+    public Vector3 velocity;
+    public float mouseSensebility = 100f;
+    Vector3 beforPosition = new Vector3(0, 0, 0);
+    float needToJump = -1;
+    float timeSincGrounded = 100;
+
+    float camXRotation;
+    float camYRotation;
+
+    public static bool CanMove = true;
+    public static bool CanLock = true;
+    public static Vector2 CamXClamp = new Vector2(-90, 90);
+    public static Vector2 CamYClamp = new Vector2(-Mathf.Infinity, Mathf.Infinity);
+    public static GameObject Player;
+
+    private void Awake()
+    {
+        Player = gameObject;
+    }
+
+    void Start()
+    {
+        beforPosition = transform.position;
+        controller = GetComponent<CharacterController>();
+        //rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    public static Vector3 InputVector
+    {
+        get { return new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Jump"), Input.GetAxisRaw("Vertical")); }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensebility * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensebility * Time.deltaTime;
+
+        camXRotation -= mouseY;
+        camYRotation += mouseX;
+
+        camXRotation = Mathf.Clamp(camXRotation, CamXClamp.x, CamXClamp.y);
+        camYRotation = Mathf.Clamp(camYRotation, CamYClamp.x, CamYClamp.y);
+
+
+        if (CanLock)
+        {
+            Camera.main.transform.localRotation = Quaternion.Euler(camXRotation, 0, 0);
+            transform.localRotation = Quaternion.Euler(0, camYRotation, 0);
+        }
+        //transform.Rotate(Vector3.up *mouseX);
+
+
+
+        Debug.DrawLine(Camera.main.transform.position, Camera.main.transform.position + Vector3.down * 0.1f, Color.red, 10);
+        Debug.DrawLine(Camera.main.transform.position, Camera.main.transform.position + Camera.main.transform.forward * 0.3f, Color.grey, 10);
+
+
+        //Interact
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+        {
+            if (hit.transform.GetComponent<Interactor>() != null)
+            {
+                if (Vector3.Distance(Camera.main.transform.position, hit.point) < 3)
+                {
+                    Debug.DrawLine(hit.point, hit.point + -Camera.main.transform.forward * 0.2f, Color.green, 5);
+                    if(Input.GetKeyDown(KeyCode.E))
+                        hit.transform.GetComponent<Interactor>().Interact.Invoke();
+                }
+                else
+                    Debug.DrawLine(hit.point, hit.point + -Camera.main.transform.forward * 0.2f, Color.blue, 5);
+            }
+            else
+                Debug.DrawLine(hit.point, hit.point + -Camera.main.transform.forward * 0.2f, Color.red, 5);
+        }
+
+
+
+        if (CanMove)
+            controller.Move(transform.right * InputVector.x * Time.deltaTime * MovementSpeed + transform.forward * InputVector.z * MovementSpeed * Time.deltaTime + Vector3.up * velocity.y * Time.deltaTime);
+
+        Vector3 colo = Vector3.Normalize(new Vector3(transform.position.x - beforPosition.x, transform.position.y - beforPosition.y, transform.position.z - beforPosition.z) + (Vector3.one * 0.5f));
+        Debug.DrawLine(beforPosition, transform.position, new Color(colo.x, colo.y, colo.z), 10);
+
+        beforPosition = transform.position;
+
+        if (Physics.Raycast(transform.position, Vector3.down, 0.7f, GroundLayer) && velocity.y < 0)
+        {
+            if (InputVector.y > 0.5f)
+            {
+                Debug.DrawLine(transform.position, transform.position + Vector3.down * 0.7f, Color.cyan, 10);
+                needToJump = 0.25f;
+            }
+            else
+                Debug.DrawLine(transform.position, transform.position + Vector3.down * 0.7f, Color.blue, 10);
+        }
+        //print((InputVector.y > 0.5f) + " " + IsGrounded());
+        if ((InputVector.y > 0.5f && (IsGrounded() || timeSincGrounded<0.1f)) || (needToJump > 0 && (IsGrounded() || timeSincGrounded < 0.1f)))
+        {
+            velocity.y = JumpPower * 2.5f;
+        }
+        else if(IsGrounded())
+        {
+            velocity = Vector3.zero;
+        }
+        else if(needToJump > 0 && !Physics.Raycast(transform.position, Vector3.down, 1.0f, GroundLayer))
+        {
+            needToJump = -1;
+        }
+        needToJump -= Time.deltaTime;
+
+        velocity += Physics.gravity / 50;
+
+        if(!IsGrounded())
+        {
+            if (timeSincGrounded == 100)
+                timeSincGrounded = 0;
+            timeSincGrounded += Time.deltaTime;
+        }
+        else
+        {
+            timeSincGrounded = 100;
+        }
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+        }
+    }
+    public bool IsGrounded()
+    {
+        //if (Physics.Raycast(transform.position, Vector3.down, 0.2f, GroundLayer))
+        if (Physics.CheckSphere(transform.position, 0.3f, GroundLayer))
+        {
+            Debug.DrawLine(transform.position, transform.position + Vector3.down * 0.3f, Color.green, 10);
+            return true;
+        }
+        else
+        {
+            Debug.DrawLine(transform.position, transform.position + Vector3.down * 0.3f, Color.red, 10);
+
+            if (needToJump < 0)
+                needToJump = -1;
+
+
+            return false;
+        }
+    }
+}
